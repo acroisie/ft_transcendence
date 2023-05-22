86c838d05b0e97219cdd1a9ee0488020fe85d118
Arnaud <acroisie@student.42lyon.fr>
2023-05-22 16:57:15 +0200

    reset project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-  },
+  plugins: [react()],
 })
